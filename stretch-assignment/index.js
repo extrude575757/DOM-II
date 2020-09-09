daf94941fdbac8d611ac233a0858ff32f72336e8
extrude575757 <nicholas.a.lacapria@gmail.com>
2020-09-08 22:59:38 -0700

    Moving rockets accross the screen

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,7 +1,18 @@
+
+
+import { gsap } from "gsap";
+import { gsapAll } from "gsap/all";
+
+if (process.client) {
+  gsap.registerPlugin(gsapAll);
+}
+
+
 let block = document.querySelectorAll('.block');
 block.forEach(e =>{
     e.addEventListener('click', (event) =>{
-        console.log(e['classList'][1])
+        // let square = document.querySelector(e['classList'][1]);
+        gsap.to('.'+e['classList'][1], {translateX: '1000',duration: 3, rotation: 360});
     });
 });
 
